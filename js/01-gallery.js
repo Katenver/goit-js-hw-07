ca5bf9847a86b975a970a7d4ced386254ced047f
Katenver <8711724@ukr.net>
2022-11-06 13:53:08 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createMarkup () {
+return galleryItems.map(({preview, original, description}) => 
+   `<div class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </div>`).join('');
+}
+galleryEl.insertAdjacentHTML('afterbegin', createMarkup ())
+
+galleryEl.addEventListener('click', openLargeImage);
+
+function openLargeImage (evt){
+evt.preventDefault();
+const instance = basicLightbox.create(`
+<img
+    src= ${evt.target.dataset.source}
+/>
+`)
+instance.show()
+}
