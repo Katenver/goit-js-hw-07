df0ec27cee01289570e2d3c89130a4cdbd639037
Katenver <8711724@ukr.net>
2022-11-06 19:21:27 +0200

    Update 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
+function createMarkup() {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    )
+    .join("");
+}
+galleryEl.insertAdjacentHTML("afterbegin", createMarkup());
+
+let simpleLightBoxGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+
+  function openLargeImage(evt) {
+    evt.preventDefault();
+  
+    if (evt.target.nodeName !== "IMG") {
+      return;
+    }
+  
+    
+    function onEscapeKeyPress(evt) {
+      if (evt.key === "Escape") {
+        instance.close();
+      }
+    }
+    galleryEl.addEventListener("keydown", onEscapeKeyPress);
+  }
 console.log(galleryItems);
